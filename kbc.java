1991421f03787232cd19f3530e21f13aabb0faca
Bharath Kumar s <114819425+BonDizard@users.noreply.github.com>
2022-11-06 12:33:01 +0530

    Update kbc.java

--- kbc.java
+++ kbc.java
@@ -17,44 +17,62 @@ public class KBC {
 	QnA.put( 8,"Who is the author of 'Manas Ka-Hans' ?");// "Amrit Lal Nagar",
 	QnA.put( 9,"The death anniversary of which of the following leaders is observed as Martyrs' Day?");// "Mahatma Oandhi",
 	QnA.put(10, "who is the Mangaka of One piece");//"Eiichiro Oda"
-	for(int i=1;i<=10;i++)
-	{
-		System.out.println(i+": the Question is:\n"+QnA.get(i)+"\n");
+	for (int i = 1; i <= 10; i++) {
+		System.out.println(i + ": the Question is:\n" + QnA.get(i) + "\n");
 		Options(i);
 		Answer(i);
 	}
 }
-	static String Answer(int b)
-	{
-		HashMap<Integer,String> a = new HashMap<Integer,String>();
-		a.put(1,"Sep8");
-		a.put(2,"Malayalam");
-		a.put(3,"Prayag. Haridwar, Ujjain,. Nasik");
-		a.put(4,"Jainism");
-		a.put(5,"Oct 14");
-		a.put(6,"Dignity for all - focus on Children'");
-		a.put(7,"World Tourism Day");
-		a.put(8,"Amrit Lal Nagar");
-		a.put(9,"Mahatma Oandhi");
-		a.put(10,"Eiichiro Oda");
-		switch(b)
+
+static String Answer(int b) {
+	HashMap<Integer, String> a = new HashMap<Integer, String>();
+
+	switch (b) {
+	case 1:
+		a.put(1, "Sep8");
+		break;
+	case 2:
+		a.put(2, "Malayalam");
+		break;
+	case 3:
+		a.put(3, "Prayag. Haridwar, Ujjain,. Nasik");
+		break;
+	case 4:
+		a.put(4, "Jainism");
+		break;
+	case 5:
+		a.put(5, "Oct 14");
+		break;
+	case 6:
+		a.put(6, "Dignity for all - focus on Children'");
+		break;
+	case 7:
+		a.put(7, "World Tourism Day");
+		break;
+	case 8:
+		a.put(8, "Amrit Lal Nagar");
+		break;
+	case 9:
+		a.put(9, "Mahatma Oandhi");
+		break;
+	case 10:
+		a.put(10, "Eiichiro Oda");
+		break;
+	}
+	return a.get(b);
+}
+static void Options(int c) {
+	String p[] = {"a","b","c","d"};
+	Random r = new Random();
+	int z = r.nextInt(4);// 2
+	p[z] = Answer(c);// 0,1,2,3
+		switch(z+1)
 		{
-		
+		case 1:System.out.println("A:"+p[z]+"\n");break;
+		case 2:System.out.println("B:"+p[z]+"\n");break;
+		case 3:System.out.println("C:"+p[z]+"\n");break;
+		case 4:System.out.println("D:"+p[z]+"\n");break;
 		}
-		return a.get(b);
-	}
 
-	
-static void Options(int c)
-	{
-		String p[];
-		Random r = new Random();
-		int z = r.nextInt(3);//2
-		p[z] = Answer(c);//0,1,2,3
-
-		
-			
-		}
-	}
-	
-
+}
+}
